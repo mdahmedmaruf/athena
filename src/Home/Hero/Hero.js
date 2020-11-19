b1fc713b343d4213b8b34de7554eff3883c436f0
mdahmedmaruf <mdahmedmaruf@gmail.com>
2020-11-19 10:18:25 +0600

    hero almost done

--- src/Home/Hero/Hero.js
+++ src/Home/Hero/Hero.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Button, Col, Container, Image, Jumbotron, Row } from 'react-bootstrap';
 import heroimage from '../../images/hero-img.png';
+import studiobotton from '../../images/studio-1.png';
 
 const Hero = () => {
     return (
-        <div className="hero-section">
+        <div className="hero-section py-5">
             <Container>
                 <Row>
                     <Col sm={12} md={6} lg={6} xl={6}>
                     <Jumbotron className="bg-transparent">
-                        <h1>Florence <br/> agency</h1>
+                        <h1>Florence <span>agency</span></h1>
                         <p>
                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                         </p>
@@ -23,6 +24,9 @@ const Hero = () => {
                     </Col>
                 </Row>
             </Container>
+            <div className="studio-bottom">
+                <Image src={studiobotton} />
+            </div>
         </div>
     );
 };
