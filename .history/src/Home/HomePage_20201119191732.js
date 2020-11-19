3613a208b3afc4291753601a13afa5f69553c43d
Your NameMdTahsinAmin <Tahsin16@cse.pstu.ac.bd.com>
2020-11-19 19:32:16 +0600

    my-two-sections

--- .history/src/Home/HomePage_20201119191732.js
+++ .history/src/Home/HomePage_20201119191732.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import About from './About/About';
-import Achievements from './Achievements/Achievements';
 import Hero from './Hero/Hero';
-import StayRunning from './StayRunning/StayRunning';
 
 const HomePage = () => {
     return (
         <>
             <Hero/>
             <About/>
-            <StayRunning></StayRunning>
-            <Achievements></Achievements>
         </>
     );
 };
